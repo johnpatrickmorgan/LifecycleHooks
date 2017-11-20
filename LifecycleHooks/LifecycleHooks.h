456adfcaf9b905c14a23a53598160055c8eca5ad
Bart Whiteley <@bwhiteley>
2017-11-19 22:32:24 -0700

    Added project files

---
+++ LifecycleHooks/LifecycleHooks.h
@@ -0,0 +1,19 @@
+//
+//  LifecycleHooks.h
+//  LifecycleHooks
+//
+//  Created by J. B. Whiteley on 11/19/17.
+//  Copyright © 2017 SwiftBit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LifecycleHooks.
+FOUNDATION_EXPORT double LifecycleHooksVersionNumber;
+
+//! Project version string for LifecycleHooks.
+FOUNDATION_EXPORT const unsigned char LifecycleHooksVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LifecycleHooks/PublicHeader.h>
+
+
