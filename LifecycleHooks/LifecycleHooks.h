2455454d562302eedba3723aff9fc1d34302ce67
Bart Whiteley <bwhiteley@gmail.com>
2017-12-28 12:17:34 -0700

    Add Carthage support. (#3)
    * Added project files
    
    * Added Carthage info to README
    
    * remove development team
    
    * Remove copyright line from new file.
    
    * Fixed org name

---
+++ LifecycleHooks/LifecycleHooks.h
@@ -0,0 +1,18 @@
+//
+//  LifecycleHooks.h
+//  LifecycleHooks
+//
+//  Created by Bart Whiteley on 11/19/17.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LifecycleHooks.
+FOUNDATION_EXPORT double LifecycleHooksVersionNumber;
+
+//! Project version string for LifecycleHooks.
+FOUNDATION_EXPORT const unsigned char LifecycleHooksVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LifecycleHooks/PublicHeader.h>
+
+
